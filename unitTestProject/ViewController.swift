6a23778a11da07becf45d7d7847257ae632d471e
bondarchuk.sergey <bondarchuk.sergey@renhealth.com>
2020-06-01 14:08:00 +0300

    first commit

---
+++ unitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  unitTestProject
+//
+//  Created by Сергей Бондарчук on 01.06.2020.
+//  Copyright © 2020 Сергей Бондарчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
